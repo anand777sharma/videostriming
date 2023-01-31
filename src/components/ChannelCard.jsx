e0187f1c35afbbafeca231bb222a89aa1899df66
Anand Sharma <84133329+anand777sharma@users.noreply.github.com>
2023-01-31 16:10:24 +0530

    ..

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,19 @@
+import {Box,CardContent,CardMedia,Typography} from '@mui/material';
+import {CheckCircle} from '@mui/icons-material';
+import{Link} from 'react-router-dom';
+import{ demoProfilePicture} from '../utils/constants';
+
+
+const ChannelCard = ({channelDetail}) => {
+ <Box 
+  sx={{
+    boxShadow:'none',
+    borderRadius:'20px'
+  }} >
+<Link>
+
+</Link>
+ </Box>
+}
+
+export default ChannelCard
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,22 @@
 import {useState,useEffect} from 'react';
 import { Box,Stack,Typography} from '@mui/material';
+
+import{fetchFromAPI} from '../utils/fetchFromAPI';
 import {Sidebar , Videos} from './';
 
 const Feed = () => {
+const [selectedCategory, setSelectedCategory] = useState('New');
+const [videos,setVideos]=useState([]);
+
+  useEffect(()=>{
+    fetchFromAPI('search?part=snippet&query=${selectedCategory}')
+    .then((data)=>setVideos(data.item));
+  },[selectedCategory]);
   return (
   <Stack sx={{flexDirection:{sx:"column",md:"row"}}}>
 <Box sx={{ height:{sx:'auto',md:'92vh'}, borderRight:'1px solid #3d3d3d',px:{sx: 0,md:2}}}>
-  <Sidebar/> 
+  <Sidebar
+  selectedCategory= {selectedCategory} setSelectedCategory ={setSelectedCategory}  /> 
   <Typography className="copyright" variant="body2" sx={{mt: 1.5,color: '#fff'}}>
     {/*39 min*/}
 Copyright 2023 Made by Anand
@@ -16,9 +26,9 @@ Copyright 2023 Made by Anand
 <Box p={2} sx={{overflowY:'auto',height: '90vh',flex: 2}}>
 
   <Typography variant="h4" fontWeight ="bold" mb={2} sx={{color:'white'}}>
-    NEW<span style={{color:'#f31503'}}>Videos</span>
+    {selectedCategory} <span style={{color:'#f31503'}}>Videos</span>
   </Typography>
-  <Videos videos={[]} />
+  <Videos videos={[videos]} />
 </Box>
   </Stack>
   )
