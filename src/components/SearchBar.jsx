e641169196d7061185216dfa21ae312b5d149298
Anand Sharma <84133329+anand777sharma@users.noreply.github.com>
2023-03-11 19:41:22 +0530

    ..

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,33 +1,45 @@
-
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from '@mui/material';
-import { Search, SearchIcon } from '@mui/icons-material';
+import SearchIcon from '@mui/icons-material/Search';
+
 const SearchBar = () => {
-    return (
-        <Paper
-            component="form"
-            onSubmit={() => { }}
-            sx={{
-                borderRadius: 20,
-                border: '1px solid #3e3e3',
-                boxShadow: 'none',
-                pl:2,
-                mr: { sm: 5 }
-            }}>
-            <input
-                className="search-bar"
-                placeholder="Search..."
-                value=""
-                onChange={() => { }}
-            />
-            <IconButton type="submit" sx={{ p: '10px', color: 'red' }}
-            >
-                <Search />
-            </IconButton>
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
 
-        </Paper>
-    )
-}
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
 
-export default SearchBar
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm('');
+    }
+  };
+
+  return (
+    <Paper
+      component='form'
+      onSubmit={onhandleSubmit}
+      sx={{
+        borderRadius: 20,
+        border: '1px solid #e3e3e3',
+        pl: 2,
+        boxShadow: 'none',
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className='search-bar'
+        placeholder='Search...'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,3 @@
-
 import MusicNoteIcon from '@mui/icons-material/MusicNote';
 import HomeIcon from '@mui/icons-material/Home';
 import CodeIcon from '@mui/icons-material/Code';
@@ -17,7 +16,7 @@ export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  { name: 'JS Mastery', icon: <CodeIcon />, },
+  // { name: 'JS Mastery', icon: <CodeIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
